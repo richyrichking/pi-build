65389015d297286dcfe19eb26cf6767299514d14
Richard King <richard.king@reece.com.au>
2025-01-25 22:38:43 +1100

    update formatting in script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 # Make a directory to run the script from
-echo "\033[32m - * - Updating APT Pacakage Lists for Repositories - * - \033[0m\n"
+echo -e "\033[32m - * - Updating APT Pacakage Lists for Repositories - * - \033[0m"
 sudo apt update
